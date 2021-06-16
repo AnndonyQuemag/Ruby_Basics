ca2ca9301f2c5c440b8cdcf2ccc6758531c5d544
Anndony Quemag <anndony.kommit@gmail.com>
2021-06-16 08:58:32 -0500

    files ready for pull request

--- Section_5.rb
+++ Section_5.rb
@@ -1,16 +1,16 @@
 #======================================================================Intro to met
-#para definir un metodo se debe empezar con def
+#to define a method you must start with def
 def introduce_myself
   puts "My name is Anndony Quemag"
   puts "My age is 23"
   puts "Im work at kommit"
 end
-#el metodo debe estar antes de llamarlo de lo contrario tendra un error el codigo, como el metoodo no tiene entradas no es necesario poner parentesis
+#The method must be there before calling it, otherwise the code will have an error, as the method has no inputs, it is not necessary to put parentheses
 introduce_myself()
 introduce_myself
 puts
 #====================================================================Local variables
-#se definen dentro de la definicion de un metodo y esta solo se ejecutara mientras el metodo se este ejecutando
+#are defined within the definition of a method and this will only be executed while the method is executing
 last_name = "Quemag1"
 def introduce_myself
   name = "Anndony"
@@ -20,10 +20,10 @@ def introduce_myself
 end
 
 introduce_myself
-#puts name #genera error ya que la variable name no existe forma global
-puts last_name #pueden tener el mismo nombre de las variables locales, pero tiende a generar confusion
+#puts name #generates error since the name variable does not exist global form
+puts last_name #they can have the same name as the local variables, but it tends to generate confusion
 #===================================================================Method parameters and arguments
-#se definen al crear el metodo en parentesis despues del nombre
+#are defined when creating the method in parentheses after the name
 def introduce_parameter(name, age)
   puts "My name is #{name}"
   puts "My age is "+age.to_s #{name}"
@@ -35,17 +35,17 @@ def add_two_numbers(num1, num2)
   puts "Solving the problem"
   return num1+num2
 end
-#add_two_numbers(12, 13)#el valor se almacena en la variable por lo cual debemos imprimir
+#add_two_numbers(12, 13)#the value is stored in the variable so we must print
 p add_two_numbers(12, 13)
 puts
 #===============================================================Return values II
-#no es necesario tener un puts dentro del metodo o un return se puede imprimir su contenido asi
+#It is not necessary to have a puts inside the method or a return, its content can be printed like this
 def return_string
   "This is a test"
 end
 
 p return_string
-#si intentamos imprimir el valor de un puts que esta dentroo de un metodo nos dara nulo ya que este devuelve el resultado de un componente del metodo
+#if we try to print the value of a puts that is inside a method it will give us null since it returns the result of a component of the method
 
 def return_string1
   puts "This is a test"
@@ -55,28 +55,28 @@ result=return_string1
 p result
 puts
 #==========================================================The if statement
-#para realizar una comparacion se utiliza if
+#to make a comparison use if
 if 5 < 7
   puts "the results is true"
 end
-#de igual forma si se quiere comparar string
+#in the same way if you want to compare string
 name ="Anndony"
 if name.downcase < "Anndony"
   puts "the results is true"
 end
 puts
-#tambien funciona para metodos
+#also works for methods
 if name.include?("don")
     puts "the results is true"
 end
 puts
 #========================================================truthiness and falsiness
-#para saber si es verdadero o falso
+#to know if it is true or falseo
 if true
   puts "That is true"
 end
 
-if 5 #el resultado es verdadero ya que aunque 5 no es booleano es un valor de verdad
+if 5 #the result is true since even though 5 is not boolean it is a true valuead
   puts "That is true"
 end
 puts
@@ -90,7 +90,7 @@ if color == "green"
   puts "Yes!!!"
 end
 puts
-#lo anterior es equivalente a lo siguiente, si color no es igual a red pasa a la siguiete condicino del elsif
+#The above is equivalent to the following, if color is not equal to red, it passes to the following condition of the elsif
 
 if color == "red"
   puts "Red Yes!!!"
@@ -101,7 +101,7 @@ elsif color == "yellow"
 end
 puts
 #=================================================If, else
-#si la condicion no cumple con las opciones pasara al else
+#if the condition does not meet the options it will go to else
 great = "c"
 if great == "a"
   puts "That's an exellent"
@@ -110,7 +110,7 @@ elsif great == "b"
 else
   puts "unacceptable!!"
 end
-#al igual que los demas se puede utilizar ddentro de un methodo
+#like the others it can be used within a method
 def condition(num1,num2)
   if num1<num2
     "Yes #{num1} < #{num2}"
@@ -121,7 +121,7 @@ end
 
 p condition(3, 2)
 #==============================================Multipĺe conditions whit &&
-#para realizar comparaciones multiples utilizando union se usa (&&)
+#to perform multiple comparisons using union use (&&)
 name = "Anndony"
 last_name = "Quemag"
 
@@ -130,12 +130,12 @@ if name == "Anndony"
     puts "Yes, this is my name"
   end
 end
-#pero podemos escribir esto en una sola linea
+#but we can write this on one line
 if name == "Anndony" && last_name == "Quemag"
   puts "Yes, this is my name"
 end
 #============================================Multipĺe conditions whit ||
-#para realizar comparaciones multiples utilizando (o) se usa (&&)
+#to perform multiple comparisons using (or) use (&&)
 word = "hello"
 other = "world"
 
@@ -144,7 +144,7 @@ if word == "hi"
     puts "Yes!!!!!!!!!"
   end
 end
-#pero podemos escribir esto en una sola linea
+#but we can write this on one line
 if word == "hello" || other == "world"
   puts "Yes!!!!!!!!!"
 end
@@ -162,9 +162,9 @@ elsif num1 > num2 && num1 > num3 && num3 < num2
   puts "the order of the numbers is: #{num3}, #{num2}, #{num1}"
 end
 #======================================================Parentheses precedence
-#cuando se va a convinar o agrupar ciertas cosas podemos utiliazar los parentesis así
+#when you are going to combine or group certain things we can use the parentheses like this
 def test(name, last_name, age)
-  if (name == "Anndony" && last_name == "Quemag") || age == "23" #esta condicion puede generar confusion por lo cual es mas recomendable utilizar comillas para que ruby entienda bien que condicion queremos poner
+  if (name == "Anndony" && last_name == "Quemag") || age == "23" #This condition can generate confusion, so it is more advisable to use quotation marks so that ruby understands well what condition we want to put
     puts "Yes, the information is correct"
   else
     puts "No! The information is false"
@@ -173,7 +173,7 @@ end
 test("Anndony", "Quemag",22)
 puts
 #=======================================================Nested if statements
-#las declaraciones if anidadas son declaraciones if dentro de otro if
+#nested if statements are if statements inside another if
 
 def meal_plane(time_of_week, time_of_day)
   if time_of_week == "weekday"
@@ -196,10 +196,76 @@ def meal_plane(time_of_week, time_of_day)
 end
 p meal_plane("weekday", "lunch")
 #===============================================The respond_to?
-#el metodo respond_to? lleva el signo de interrrogacion al final lo que indica que es un metodo booleanoque retornara true o false, para esto se debe proporcionar un argumento que sera el nombre de otro metodo
+#the method respond_to? It has the question mark at the end which indicates that it is a Boolean method that will return true or false, for this an argument must be provided that will be the name of another method
 num = 1000
 
 p num.respond_to?("class")
 if num.respond_to?("next")
-  num.next
+  p num.next
 end
+#This method is used to check if something exists in the object and thus be able to continue workingo
+puts "hello".respond_to?("length")
+puts "hello".respond_to?(:length)#without having to put quotes, the colon is used to put the method
+#==============================================The ternary operator
+#we can replace comparisons with a ternary operator, which is so called because there are three components or sections and each one is separated by a sign
+if 1<2
+  puts "Yes, it is"
+else
+  puts "No, it's not"
+end
+puts
+puts 1 < 2 ? "yes, it is" : "No, it's not" #What should be done is to make a comparison followed by a question mark (?) To indicate if it is true and after this two points to indicate that it is not true (:)
+#otro ejemplo
+puts "Hello".downcase == "hello" ? "The two are equal" : "The two are't equal"
+def test(num)
+  num.even? ? "That number is even" : "That number is odd"
+end
+puts test(13)
+#With this method, lines of code are saved since the entire comparison is equal to an if, so when performed in a line
+puts
+#=======================================================Default or optional parameters
+#the default parameters, what it is about is that an argument of a ruby method most of the time expects that they send a value of the same type of the argument, so a parameter can be provided a default value that will be used as a specific parameter using the same like this
+def make_phone(num, code = 1, area = 10)
+  puts "calling #{num}, whit code #{code} - #{area}"
+end
+
+make_phone(12345656) #in this case only one argument of three is sent, but the result does not generate any error since specific parameters have been defined
+puts
+#=======================================================Call a method from another method
+#we can call a method within another method by interpolation like this
+def add(a,b)
+  a+b
+end
+def substract(a, b)
+  a-b
+end
+def multiply(a,b)
+  a*b
+end
+
+def calculator(a = 1,b = 1, operation = "add")
+  if operation == "add"
+    add(a, b)
+  elsif operation == "substract"
+    substract(a, b)
+  elsif operation == "multiply"
+    multiply(a, b)
+  else
+    "That's not a real math operation"
+  end
+end
+
+#p calculator(1,2,"add")#This calls the calculator method and this method in turn makes a comparison to call other methods, it must be taken into account that the methods must be defined before calling them otherwise we will have an error
+#if we want to call the method with a message we can do it like this
+def calculator(a = 1,b = 1, operation = "add")
+  if operation == "add"
+    "The result of operation #{a} + #{b} is: #{add(a, b)}"
+  elsif operation == "substract"
+    "The result of operation #{a} - #{b} is: #{substract(a, b)}"
+  elsif operation == "multiply"
+    "The result of operation #{a} * #{b} is: #{multiply(a, b)}"
+  else
+    "That's not a real math operation"
+  end
+end
+p calculator(4, 2, "multiply")
---
+++ Section_6.rb
@@ -0,0 +1,171 @@
+#==================================================The case Statement
+#This declaration allows us to replace the if and else if operations, it is advisable to use this declaration when using many nested ifs such as three or more, using a case and a when like this
+def my_food(food = "hamburguer".capitalize)
+  case food
+  when "Steak"
+    "Ohh, delicious"
+  when "Sushi"
+    "ummm, I don't like"
+  when "Pizza"
+    "The pizza es very delicious"
+  when "Hamburguer", "Chips", "Chicken nuggets"#we can put multiple comparisons separated by a comma for a specific message saving comparators like && and saving space without if
+    "Ohh, mi favorite food"
+  else
+    "I'm sorry, I don't know about that food"
+  end
+end
+
+p my_food("potatoes".capitalize)
+
+def calculate_number(num = 14)
+  case num
+  when 1 .. 10# calculates if the number sent by parameter is between 1 and 10 with the double point method (..)
+    "Range one"
+  when 11 .. 20
+    "Range two"
+  when 21 .. 30 then "range_three"
+  else
+    "The number no exist in the Range"
+  end
+end
+
+p calculate_number(23)
+# we can also use then to show the messages and save lines of code
+def calculate_number_one(num = 14)
+  case num
+  when 1 .. 10 then"Range one"
+  when 11 .. 20 then"Range two"
+  when 21 .. 30 then "Range_three"
+  else "The number no exist in the Range"
+  end
+end
+p calculate_number_one(7)
+puts
+#============================================================Negation with (!)
+#Negation is expressed with the exclamation point (!)
+puts !false # as you can see the negation of false will give us a result that in this case is true
+puts !1 #a true value is being denied so it returns a false
+puts !""
+puts !!false #in this case we start denying something and after that we deny it again it is like canceling the first negation
+#===========================================================the unless keyword
+#the word unless is used, which is used to execute if it is false, then to show how it works we will compare it with an if
+
+password = "test"
+
+if password != "dog"
+  puts "Not allowed"
+else
+  puts "That's the right password, Welcome!..."
+end
+puts
+unless password == "dog"#in this case when using unless we are negating the equal comparator (==), then what we would say is not if it is equal do this, we would say if it is unless this is equal do
+  puts "Not allowed"
+else
+  puts "That's the right password, Welcome!..."
+end
+puts
+if !password.include?("a")
+  puts "It does not include the letter"
+end
+#=========================================================The while keyword
+#use while to create a loop that will repeat itself over and over again
+i = 1
+while i <= 10#everything that is inside the while is executed as long as the condition is correct
+  puts "Iteration: #{i}"
+  i+=1
+end
+=begin
+status = true
+
+while status
+  print "Please enter username: "
+  username = gets.chomp.downcase
+  print "Please enter password: "
+  password = gets.chomp
+
+  if username == "anndony" && password == "123456789"
+    puts "Yes!! User confirmed, Welcom..."
+    status= false
+  elsif username == "quit" || password == "quit"
+    puts "Goodbay. Better luck next time"
+    status = false
+  else
+      puts "Incorrect combination, try again or enter 'quit'"
+  end
+end
+=end
+puts
+#=========================================================the until keywordoar
+#A loop will be created that can be like a complement to the while loop but this time with until, but this is executed in the opposite way to while like this
+i = 1
+while i < 10
+  puts i
+  i+=1
+end
+i = 1
+puts "Ciclo Until"
+#now if we want to take this loop to an until loop
+until i > 9 #until is executed while the condition is false, when the condition is true the loop ends
+  puts i
+  i+=1
+end
+puts
+#==========================================================Fizzbuzz
+#This is a type of problem that most of the time is easy to solve but it can become much more complicated than it seems.
+def fizzbuzz(num)
+  aux = 1
+  while aux <= num
+    if aux % 3 == 0 && aux % 5 == 0
+      puts "The number #{aux} is divided by 3 y 5 'Fizzbuzz'"
+    elsif aux % 3 == 0
+      puts "The number #{aux} is divided by 3 'Fizz'"
+    elsif aux % 5 == 0
+      puts "EThe number #{aux} is divided by 5  'Buzz'"
+
+    end
+    aux+=1
+  end
+end
+
+fizzbuzz(45)
+puts
+#=======================================================Statement modifiers
+#line modifiers work to modify what we write as an if, it serves as a shorthand syntax to decrease the number of lines of code
+vara = 3
+ver = true
+if vara < 10 && ver
+  puts "yes, the number is less than 10"
+end
+#to use the line modifier in this case we must put the message at the beginning and follow the if condition separated by a space
+puts "yes, the number is less than 10" if vara < 10 && ver
+#in the same way we can do this with unless
+puts
+unless vara > 10 && !ver
+  puts "yes, the number is less than 10"
+end
+puts "yes, the number is less than 10" unless vara > 10 && !vera
+puts
+#=================================================Conditional Assingment
+#the conditional assignment operator allows assigning a new value to a variable if and only if the value of said variable is null
+varb = nil
+p varb
+#we can easily assign a new value to varb with equal (=), so the difference between this assignment and using the conditional assignment (||) is that it will only be executed if the value of the variable is nil
+varb = 7
+p varb
+varb = 10#the value 10 was assigned to the variable varb even though a value already existed previously (7)
+p varb
+
+varc = nil
+varc ||= 20#since varc is nil the value 20 is assigned
+p varc
+
+#si intentamos usar la asignacion condicional a varb que no es nil el valor no se asignara
+varb ||= 20
+p varb
+puts
+#This is recommended when looking for a position that does not exist in some arrangement or word
+word = "Hello"
+pos = 4
+result = word[pos]
+result ||="Not Found"
+p result
